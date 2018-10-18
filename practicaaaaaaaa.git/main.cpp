7c7f5467b22634a436bf4d1f844599d187ad8ad3
DiegoRamirezUCSP <42581054+DiegoRamirezUCSP@users.noreply.github.com>
2018-10-17 19:55:35 -0500

    first commit

---
+++ practicaaaaaaaa.git/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "todq.h"
+using namespace std;
+
+int main()
+{
+   puntos vector1;
+   char x = 'ñ';
+    int a=vector1.start.x = 13;
+    int b=vector1.endd.x = 21;
+    int c=vector1.start.y= 12;
+    int d=vector1.endd.y = 41;
+    cout << x << endl;
+    cout << "el vector x va desde " << a << " hasta " << b <<   endl;
+    cout << "el vector y va desde " << c << " hasta " << d <<   endl;
+
+    return 0;
+   }
